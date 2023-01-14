2846532424e3952ed717edac61ec14ca39262eb0
YM CHOI <escriboy@gmail.com>
2023-01-14 22:03:44 +0900

    Context.value

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Provider를 사용할 때는 value값을 명시해 주어야 한다. */}
+      <ColorContext.Provider value={{ color: "red" }}>
+        <div>
+          <ColorBox />
+        </div>
+      </ColorContext.Provider>
     </div>
   );
 }
-
-export default App;
