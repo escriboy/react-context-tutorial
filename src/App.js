ec43a64d2171c5f4663a29c95289f35380afd8fb
YM CHOI <escriboy@gmail.com>
2023-01-14 22:47:42 +0900

    SelectColors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import ColorBox from "./components/ColorBox";
-import ColorContext from "./contexts/color";
+import ColorBox from './components/ColorBox';
+import { ColorProvider } from './contexts/color';
+import SelectColors from './components/SelectColors';
 
 export default function App() {
   return (
     <div>
       {/* Provider를 사용할 때는 value값을 명시해 주어야 한다. */}
-      <ColorContext.Provider value={{ color: "red" }}>
+      <ColorProvider>
         <div>
+          <SelectColors />
           <ColorBox />
         </div>
-      </ColorContext.Provider>
+      </ColorProvider>
     </div>
   );
 }
